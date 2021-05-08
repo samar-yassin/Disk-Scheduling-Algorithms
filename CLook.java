734c768b10fe04ef58757d5a620b7169e6dc9ac2
samar <samareeyassin155@gmail.com>
2021-05-08 06:46:54 +0200

    added direction

--- CLook.java
+++ CLook.java
@@ -3,56 +3,53 @@ import java.util.Collections;
 
 public class CLook {
 
-	static ArrayList<Integer> sequence = new ArrayList<Integer>();
-	
-	public static int calculateTotalSeekTime(ArrayList<Integer> Requests, int head){
-		sequence.clear();
-		ArrayList<Integer>left = new ArrayList<Integer>() ,  right=new ArrayList<Integer>();
-		int seekTrack;
-
-		int totalHeadMovement=0;
-
+	public static int calculateTotalSeekTime(ArrayList<Integer> Requests, int headstart,int direction){
 		System.out.println("CLOOK ALGORITHM");
 		System.out.println("the sequence of head movement :");
+		int seekTrack;
+		int totalHeadMovement=0;
+		int head=headstart;
+		Collections.sort(Requests);
 
-		for(int i=0 ;i <Requests.size() ; i++){
-			if(Requests.get(i) <= head)
-				left.add(Requests.get(i));
-			else
-				right.add(Requests.get(i));
+		if(direction==1) {
+			for (int i = 0; i < Requests.size(); i++) {
+				seekTrack = Requests.get(i);
+				if (seekTrack >= headstart) {
+					System.out.print(seekTrack + " ");
+					totalHeadMovement += Math.abs(seekTrack - head);
+					head = seekTrack;
+				}
+			}
+
+			for (int i = 0; i < Requests.size(); i++) {
+				seekTrack = Requests.get(i);
+				if (seekTrack < headstart) {
+					System.out.print(seekTrack + " ");
+					totalHeadMovement += Math.abs(seekTrack - head);
+					head = seekTrack;
+				}
+			}
 		}
+		else if(direction==2){
+			for (int i = Requests.size()-1; i>=0; i--) {
+				seekTrack = Requests.get(i);
+				if (seekTrack <= headstart) {
+					System.out.print(seekTrack + " ");
+					totalHeadMovement += Math.abs(seekTrack - head);
+					head = seekTrack;
+				}
+			}
 
-		Collections.sort(right);
-		Collections.sort(left);
+			for (int i = Requests.size()-1; i>=0; i--) {
+				seekTrack = Requests.get(i);
+				if (seekTrack > headstart) {
+					System.out.print(seekTrack + " ");
+					totalHeadMovement += Math.abs(seekTrack - head);
+					head = seekTrack;
+				}
+			}
 
-
-		for(int i=0 ;i <right.size() ; i++){
-			seekTrack= right.get(i);
-			sequence.add(seekTrack);
-			System.out.print(seekTrack + " ");
-
-			totalHeadMovement += Math.abs(seekTrack - head);
-
-			head =seekTrack;
 		}
-
-
-		for(int i=0 ;i <left.size() ; i++){
-			seekTrack= left.get(i);
-			sequence.add(seekTrack);
-			System.out.print(seekTrack + " ");
-
-			totalHeadMovement += Math.abs(seekTrack - head);
-
-
-
-			head =seekTrack;
-		}
-
 		return totalHeadMovement;
-	}
-	
-	public static ArrayList<Integer> getSequence() {
-		return sequence;
 	}
 }
