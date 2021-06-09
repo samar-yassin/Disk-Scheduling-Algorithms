9afb15bd8226e3fdf05f5f350bd9d1054d16b71f
Marwa Omar <farabi.marwa@gmail.com>
2021-06-09 02:19:09 +0200

    Delete AllocationTechniques.java

--- AllocationTechniques.java
+++
@@ -1,8 +0,0 @@
-public interface AllocationTechniques {
-
-    void allocate(FreeSpaceManger manger , File1 f);
-    void deallocate(FreeSpaceManger manger, File1 f);
-    public String toString();
-
-
-    }
